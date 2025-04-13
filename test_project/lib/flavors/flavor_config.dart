31c983f42f0d895fb5a9ff4c78e75725105a7429
5tepan <f.stepa.n@yandex.ru>
2025-04-13 16:19:30 +0300

    Добавил и настроил flavors

---
+++ test_project/lib/flavors/flavor_config.dart
@@ -0,0 +1,23 @@
+enum Flavor { dev, prod }
+
+class FlavorConfig {
+  final Flavor flavor;
+  final String name;
+
+  static late FlavorConfig _instance;
+
+  factory FlavorConfig({
+    required Flavor flavor,
+    required String name,
+  }) {
+    _instance = FlavorConfig._internal(flavor, name);
+    return _instance;
+  }
+
+  FlavorConfig._internal(this.flavor, this.name);
+
+  static FlavorConfig get instance => _instance;
+
+  static bool isDev() => _instance.flavor == Flavor.dev;
+  static bool isProd() => _instance.flavor == Flavor.prod;
+}
--- test_project/lib/main.dart
+++ test_project/lib/main.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:test_project/core/router/app_router.dart';
 import 'package:test_project/core/theme/theme_builder.dart';
+import 'package:test_project/flavors/flavor_config.dart';
 
 void main() {
+  FlavorConfig(
+    flavor: Flavor.dev,
+    name: "Dev",
+  );
+
   runApp(
     ProviderScope(
       child: MyApp(),
